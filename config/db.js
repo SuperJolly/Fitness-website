d063558f5ba6e43c108879bfdd5a4eb94a3dddab
superjolly <1328062379@qq.com>
2017-05-13 13:36:49 +0800

    发布配置

---
+++ config/db.js
@@ -0,0 +1,31 @@
+require('../data/utils')
+const mongoose = require('mongoose')
+
+const dbConfig =  {
+  db: "mongodb://jeremygao:jeremygao@localhost:27017/Artlist",
+  options: {
+  	server: {
+    	auto_reconnect: true,
+    	poolSize: 10
+   	}
+  }
+}
+
+
+
+try{
+  mongoose.connect(dbConfig.db, dbConfig.options)
+} catch(err) {
+  mongoose.createConnection(dbConfig.db, dbConfig.options)
+}
+mongoose.connection.on('connected', function() {
+  console.log('Mongoose database connected to: ' + dbConfig.db)
+})
+mongoose.connection.on('error', function(err) {
+  console.log('Mongoose connection error: ' + err)
+})
+mongoose.connection.on('disconnected', function() {
+  console.log('Mongoose connection disconnected!')
+})
+
+module.exports = mongoose
