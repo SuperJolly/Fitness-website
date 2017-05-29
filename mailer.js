053b007d95e1a76d51a5a9d4ce13666af1e6278c
superjolly <1328062379@qq.com>
2017-05-29 17:59:01 +0800

    disabled private info

--- mailer.js
+++ mailer.js
@@ -7,24 +7,24 @@ let weekDate = ''
 
 //建立周计划
 const rule = new schedule.RecurrenceRule()
-rule.dayOfWeek = 5
-rule.hour = 12
-rule.minute = 0
+rule.dayOfWeek = 1
+rule.hour = 16
+rule.minute = 59
 rule.second = 0
 schedule.scheduleJob(rule, () => {
 	const smtpTransport = nodemailer.createTransport({
-		host: 'smtp.ym.163.com',
+		host: 'xxxx',
 		secureConnection: false,
 		port: 25,
 		auth: {
-			user: 'jeremydk@www.jeremydk.cn',
-			pass: 'Dongke@me'
+			user: 'xxxx',
+			pass: 'xxxx'
 		}
 	})
 	const mailOptions = {
-		from: "jeremydk@www.jeremydk.cn",
+		from: "xxxx",
 		to: '',
-		subject: 'mail of fitness system ',
+		subject: 'xxxx',
 		html: ''
 	}
 
@@ -36,6 +36,7 @@ schedule.scheduleJob(rule, () => {
 		    const uname = p.name
 		    let counts = [0,0,0,0]
 		    let planNum = [0,0,0,0]
+		    let completeRate = [0,0,0,0]
 		    p.dayComplete.forEach((v) => {
 		      if(parseInt(v.timeId) >= parseInt(weekDate)) {             //符合条件的item
 		        v.items.forEach((s) => {
@@ -50,15 +51,27 @@ schedule.scheduleJob(rule, () => {
 		  			})
 		  		}
 		  	})
-		  	mailOptions.html = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'+uname+', 您好: </br>'+
-		  			'&emsp;&emsp;下面为【'+uname+'】本周健身周报汇报内容</br>'+
-		  			'<ul>'+
-		  				'<li> 引体向上周计划值为：'+'<strong>'+planNum[0]+'</strong>'+', 本周完成'+(counts[0]/planNum[0]*100).toFixed(2)+'%'+'</li>'+
-		  				'<li> 俯卧撑周计划值为：'+'<strong>'+planNum[1]+'</strong>'+', 本周完成'+(counts[1]/planNum[1]*100).toFixed(2)+'%'+'</li>'+
-		  				'<li> 仰卧起坐周计划值为：'+'<strong>'+planNum[2]+'</strong>'+', 本周完成'+(counts[2]/planNum[2]*100).toFixed(2)+'%'+'</li>'+
-		  				'<li> 深蹲周计划值为：'+'<strong>'+planNum[3]+'</strong>'+', 本周完成'+(counts[3]/planNum[3]*100).toFixed(2)+'%'+'</li>'+
-		  			'</ul></br>'+
-		  			'希望您继续保持，身体健康! :)'
+		  	if(!planNum[0] && !planNum[1] && !planNum[2] && !planNum[3]) {
+		  		mailOptions.html = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'+uname+', 您好: </br>'+
+		  			'&emsp;&emsp;您本周尚未设置任何健身项目周计划,请您尽快设置，以确保正常反馈您的健身情况! :)'
+		  	} else {
+		  		planNum.forEach((item, i) => {
+		  			if(item === 0) {
+		  				completeRate[i] = 0
+		  			} else {
+		  				completeRate[i] = (counts[i]/item*100).toFixed(2)
+		  			}
+		  		})
+			  	mailOptions.html = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'+uname+', 您好: </br>'+
+			  			'&emsp;&emsp;下面为【'+uname+'】本周健身周报汇报内容</br>'+
+			  			'<ul>'+
+			  				'<li> 引体向上周计划值为：'+'<strong>'+planNum[0]+'</strong>'+', 本周完成'+completeRate[0]+'%'+'</li>'+
+			  				'<li> 俯卧撑周计划值为：'+'<strong>'+planNum[1]+'</strong>'+', 本周完成'+completeRate[1]+'%'+'</li>'+
+			  				'<li> 仰卧起坐周计划值为：'+'<strong>'+planNum[2]+'</strong>'+', 本周完成'+completeRate[2]+'%'+'</li>'+
+			  				'<li> 深蹲周计划值为：'+'<strong>'+planNum[3]+'</strong>'+', 本周完成'+completeRate[3]+'%'+'</li>'+
+			  			'</ul></br>'+
+			  			'希望您继续保持，身体健康! :)'
+	  		}
 		  	mailOptions.to = p.mail
 
 		  	smtpTransport.sendMail(mailOptions, function(err, res) {
